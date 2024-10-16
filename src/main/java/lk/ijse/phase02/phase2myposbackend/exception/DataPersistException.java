22271588c9c08b9431b3b9a96f60d813e465a00c
Charutha-Herath <charuh2023@gmail.com>
2024-10-16 23:26:19 +0530

    add DataPersistException

---
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/exception/DataPersistException.java
@@ -0,0 +1,13 @@
+package lk.ijse.phase02.phase2myposbackend.exception;
+
+public class DataPersistException extends RuntimeException {
+    public DataPersistException() {
+    }
+
+    public DataPersistException(String message) {
+
+    }
+    public DataPersistException(String message, Throwable cause) {
+
+    }
+}
