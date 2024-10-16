a900cbd5071b37db211753faa3c16bf09ab13605
Charutha-Herath <charuh2023@gmail.com>
2024-10-16 23:31:03 +0530

    add UserDao

---
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/dao/UserDao.java
@@ -0,0 +1,11 @@
+package lk.ijse.phase02.phase2myposbackend.dao;
+
+
+import lk.ijse.phase02.phase2myposbackend.entity.impl.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDao extends JpaRepository<UserEntity,String> {
+    UserEntity findByEmail(String email);
+}
