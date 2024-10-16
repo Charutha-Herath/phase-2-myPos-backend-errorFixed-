986ce17c1607fcbd184ed9bf9a2cde92e64fb1a7
Charutha-Herath <charuh2023@gmail.com>
2024-10-16 21:54:29 +0530

    done WebAppConfig

---
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/config/WebAppConfig.java
@@ -0,0 +1,15 @@
+package lk.ijse.phase02.phase2myposbackend.config;
+
+
+import jakarta.servlet.annotation.MultipartConfig;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.phase02.phase2myposbackend")
+@EnableWebMvc
+@MultipartConfig
+public class WebAppConfig {
+
+}
