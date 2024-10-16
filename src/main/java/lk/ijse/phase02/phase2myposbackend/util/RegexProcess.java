59eb94091ec48069066128afb5cb244cca0c255a
Charutha-Herath <charuh2023@gmail.com>
2024-10-16 23:28:38 +0530

    add RegexProcess

---
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/util/RegexProcess.java
@@ -0,0 +1,12 @@
+package lk.ijse.phase02.phase2myposbackend.util;
+import java.util.regex.Pattern;
+
+public class RegexProcess {
+
+    public static boolean userEmailMatcher(String email) {
+        String regexForUserEmail = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$\n";
+        Pattern regexPattern = Pattern.compile(regexForUserEmail);
+        return regexPattern.matcher(email).matches();
+    }
+
+}
