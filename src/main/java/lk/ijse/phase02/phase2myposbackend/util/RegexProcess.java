5e1a1cf852f3b555d6d66ed97dbbc65e5a0b8dfd
Charutha-Herath <charuh2023@gmail.com>
2024-10-17 14:02:23 +0530

    updated email regex

--- src/main/java/lk/ijse/phase02/phase2myposbackend/util/RegexProcess.java
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/util/RegexProcess.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 public class RegexProcess {
 
     public static boolean userEmailMatcher(String email) {
-        String regexForUserEmail = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$\n";
+        String regexForUserEmail = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
         Pattern regexPattern = Pattern.compile(regexForUserEmail);
         return regexPattern.matcher(email).matches();
     }
