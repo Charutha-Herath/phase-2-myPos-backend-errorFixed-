c7bb8b3be2662eb3210c34bf8d1946b4eaa17a0b
Charutha-Herath <charuh2023@gmail.com>
2024-10-17 15:58:39 +0530

    add CustomerService with configurations

---
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/service/CustomerService.java
@@ -0,0 +1,8 @@
+package lk.ijse.phase02.phase2myposbackend.service;
+
+import lk.ijse.phase02.phase2myposbackend.dto.impl.CustomerDTO;
+
+public interface CustomerService {
+
+    void saveCustomer(CustomerDTO customerDTO);
+}
