9b1a930cdd377d6b9e5b5c696639ed1e65804c10
Charutha-Herath <charuh2023@gmail.com>
2024-10-17 16:00:26 +0530

    updated mapping customer map

--- src/main/java/lk/ijse/phase02/phase2myposbackend/util/Mapping.java
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/util/Mapping.java
@@ -2,7 +2,9 @@ package lk.ijse.phase02.phase2myposbackend.util;
 
 
 
+import lk.ijse.phase02.phase2myposbackend.dto.impl.CustomerDTO;
 import lk.ijse.phase02.phase2myposbackend.dto.impl.UserDTO;
+import lk.ijse.phase02.phase2myposbackend.entity.impl.CustomerEntity;
 import lk.ijse.phase02.phase2myposbackend.entity.impl.UserEntity;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,8 +19,9 @@ public class Mapping {
         return modelMapper.map(userDTO, UserEntity.class);
     }
 
-    public UserDTO toUserDTO(UserEntity userEntity) {
-        return modelMapper.map(userEntity, UserDTO.class);
-    }
+    public UserDTO toUserDTO(UserEntity userEntity) {return modelMapper.map(userEntity, UserDTO.class);}
 
+    public CustomerEntity toCustomerEntity(CustomerDTO customerDTO){return modelMapper.map(customerDTO, CustomerEntity.class);}
+
+    public CustomerDTO toCustomerDTO(CustomerEntity customerEntity){return modelMapper.map(customerEntity,CustomerDTO.class);}
 }
