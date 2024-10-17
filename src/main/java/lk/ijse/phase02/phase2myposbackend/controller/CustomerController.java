412753463a033ec4f282ff11591c4b0bf26c5a9f
Charutha-Herath <charuh2023@gmail.com>
2024-10-17 16:33:27 +0530

    add generateNewId

--- src/main/java/lk/ijse/phase02/phase2myposbackend/controller/CustomerController.java
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/controller/CustomerController.java
@@ -1,7 +1,9 @@
 package lk.ijse.phase02.phase2myposbackend.controller;
 
+import lk.ijse.phase02.phase2myposbackend.dto.CustomerStatus;
 import lk.ijse.phase02.phase2myposbackend.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +13,11 @@ public class CustomerController {
 
     @Autowired
     private CustomerService customerService;
+
+    @GetMapping("generateNextCustomerId")
+    public CustomerStatus generateNewId(){
+        return customerService.getNextNewId();
+
+    }
 
 }
