0718386660af15487046794a6abad2c96f6a8866
Charutha-Herath <charuh2023@gmail.com>
2024-10-17 15:15:21 +0530

    add CustomerController class

---
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/controller/CustomerController.java
@@ -0,0 +1,11 @@
+package lk.ijse.phase02.phase2myposbackend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/customer")
+public class CustomerController {
+
+
+}
