7c8753f40872464a46dea6472b224321928f7c7f
Charutha-Herath <charuh2023@gmail.com>
2024-10-17 15:58:06 +0530

    add CustomerEntity with configurations

---
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/entity/impl/CustomerEntity.java
@@ -0,0 +1,24 @@
+package lk.ijse.phase02.phase2myposbackend.entity.impl;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "customer")
+public class CustomerEntity implements Serializable {
+
+    @Id
+    private String customerId;
+    private String customerName;
+    private String contact;
+    private String address;
+}
