1980b652a0a1525491dcc1cbcab1d6cf465576e4
Charutha-Herath <charuh2023@gmail.com>
2024-10-17 16:35:36 +0530

    add getNextNewId method

--- src/main/java/lk/ijse/phase02/phase2myposbackend/service/CustomerService.java
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/service/CustomerService.java
@@ -1,8 +1,11 @@
 package lk.ijse.phase02.phase2myposbackend.service;
 
+import lk.ijse.phase02.phase2myposbackend.dto.CustomerStatus;
 import lk.ijse.phase02.phase2myposbackend.dto.impl.CustomerDTO;
 
 public interface CustomerService {
 
     void saveCustomer(CustomerDTO customerDTO);
+
+    CustomerStatus getNextNewId();
 }
