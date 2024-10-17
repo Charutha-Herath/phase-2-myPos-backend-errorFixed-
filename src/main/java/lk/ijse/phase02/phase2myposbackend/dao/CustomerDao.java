0da527d4cb2e5b7cb3aeef88a9c92d8279841291
Charutha-Herath <charuh2023@gmail.com>
2024-10-17 16:34:04 +0530

    add generateNewId native query

--- src/main/java/lk/ijse/phase02/phase2myposbackend/dao/CustomerDao.java
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/dao/CustomerDao.java
@@ -1,9 +1,15 @@
 package lk.ijse.phase02.phase2myposbackend.dao;
 
 import lk.ijse.phase02.phase2myposbackend.entity.impl.CustomerEntity;
+import lk.ijse.phase02.phase2myposbackend.entity.impl.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerDao extends JpaRepository<CustomerEntity,String> {
+
+    @Query(value = "SELECT MAX(customerId) AS last_customer_id FROM customer", nativeQuery = true)
+    CustomerEntity generateNextNewId();
 }
