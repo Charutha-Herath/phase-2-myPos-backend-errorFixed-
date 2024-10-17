9540de52d00afce982a140bf6f378fe91cba7206
Charutha-Herath <charuh2023@gmail.com>
2024-10-17 14:04:11 +0530

    done native query findByEmail

--- src/main/java/lk/ijse/phase02/phase2myposbackend/dao/UserDao.java
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/dao/UserDao.java
@@ -3,9 +3,15 @@ package lk.ijse.phase02.phase2myposbackend.dao;
 
 import lk.ijse.phase02.phase2myposbackend.entity.impl.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserDao extends JpaRepository<UserEntity,String> {
-    //UserEntity findByEmail(String email);
+
+    @Query(value = "SELECT * FROM user WHERE email = :email", nativeQuery = true)
+    UserEntity findByEmail(@Param("email") String email);
+
+
 }
