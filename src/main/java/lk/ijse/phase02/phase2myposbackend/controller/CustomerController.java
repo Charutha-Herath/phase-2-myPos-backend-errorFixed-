d47361697916ded754dfb2b0365aaf65ebab5ce6
Charutha-Herath <charuh2023@gmail.com>
2024-10-17 15:56:09 +0530

    add CustomerController annotations

--- src/main/java/lk/ijse/phase02/phase2myposbackend/controller/CustomerController.java
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/controller/CustomerController.java
@@ -1,5 +1,7 @@
 package lk.ijse.phase02.phase2myposbackend.controller;
 
+import lk.ijse.phase02.phase2myposbackend.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/customer")
 public class CustomerController {
 
+    @Autowired
+    private CustomerService customerService;
 
 }
