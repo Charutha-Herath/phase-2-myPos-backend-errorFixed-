ae48c44716c3ad5b9df60b16a09cb3cade3e5419
Charutha-Herath <charuh2023@gmail.com>
2024-10-19 22:45:07 +0530

    done generateNextCustomerId

--- src/main/java/lk/ijse/phase02/phase2myposbackend/controller/CustomerController.java
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/controller/CustomerController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.ServerSocket;
+
 @RestController
 @RequestMapping("api/v1/customer")
 public class CustomerController {
