f0e73761e548df8a0dfa6267f101c0c1cf6a9203
Charutha-Herath <charuh2023@gmail.com>
2024-10-20 14:17:14 +0530

    done update customer implementation in CustomerController

--- src/main/java/lk/ijse/phase02/phase2myposbackend/controller/CustomerController.java
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/controller/CustomerController.java
@@ -2,8 +2,10 @@ package lk.ijse.phase02.phase2myposbackend.controller;
 
 import lk.ijse.phase02.phase2myposbackend.dto.CustomerStatus;
 import lk.ijse.phase02.phase2myposbackend.dto.impl.CustomerDTO;
+import lk.ijse.phase02.phase2myposbackend.exception.CustomerNotFoundException;
 import lk.ijse.phase02.phase2myposbackend.exception.DataPersistException;
 import lk.ijse.phase02.phase2myposbackend.service.CustomerService;
+import lk.ijse.phase02.phase2myposbackend.util.RegexProcess;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -32,6 +34,25 @@ public class CustomerController {
             customerService.saveCustomer(customerDTO);
             return new ResponseEntity<>(HttpStatus.CREATED);
         }catch (DataPersistException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping(value = "/{customerId}")
+    public ResponseEntity<Void> updateCustomer(@PathVariable ("customerId") String custId,
+                                           @RequestBody CustomerDTO updatedCustomerDTO){
+        //validations
+        try {
+            if(!RegexProcess.customerIdMatcher(custId) || updatedCustomerDTO == null){
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            customerService.updateCustomer(custId,updatedCustomerDTO);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (CustomerNotFoundException e){
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }catch (Exception e){
