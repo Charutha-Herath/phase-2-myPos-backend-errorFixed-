3f1cee259fbd4ba830bd1b3f800e974c8de30027
Charutha-Herath <charuh2023@gmail.com>
2024-10-20 12:50:08 +0530

    done save customer implementation

--- src/main/java/lk/ijse/phase02/phase2myposbackend/controller/CustomerController.java
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/controller/CustomerController.java
@@ -1,11 +1,14 @@
 package lk.ijse.phase02.phase2myposbackend.controller;
 
 import lk.ijse.phase02.phase2myposbackend.dto.CustomerStatus;
+import lk.ijse.phase02.phase2myposbackend.dto.impl.CustomerDTO;
+import lk.ijse.phase02.phase2myposbackend.exception.DataPersistException;
 import lk.ijse.phase02.phase2myposbackend.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.ServerSocket;
 
@@ -20,6 +23,21 @@ public class CustomerController {
     public CustomerStatus generateNewId(){
         return customerService.getNextNewId();
 
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveCustomer(@RequestBody CustomerDTO customerDTO) {
+        try {
+            customerService.saveCustomer(customerDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }catch (DataPersistException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
