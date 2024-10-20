aa38848e9b09c7ed6e6d43ae81b45597f2202001
Charutha-Herath <charuh2023@gmail.com>
2024-10-20 20:16:16 +0530

    updated ItemDTO

--- src/main/java/lk/ijse/phase02/phase2myposbackend/dto/impl/ItemDTO.java
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/dto/impl/ItemDTO.java
@@ -1,4 +1,17 @@
 package lk.ijse.phase02.phase2myposbackend.dto.impl;
 
-public class ItemDTO {
+import lk.ijse.phase02.phase2myposbackend.dto.ItemStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ItemDTO implements ItemStatus {
+
+    private String itemCode;
+    private String description;
+    private String qty;
+    private String price;
 }
