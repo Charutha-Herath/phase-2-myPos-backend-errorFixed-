5a41af5a4a1255789e60e7f261e927fba6856c9b
Charutha-Herath <charuh2023@gmail.com>
2024-10-20 20:11:21 +0530

    add autowired

--- src/main/java/lk/ijse/phase02/phase2myposbackend/controller/ItemController.java
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/controller/ItemController.java
@@ -1,9 +1,14 @@
 package lk.ijse.phase02.phase2myposbackend.controller;
 
+import lk.ijse.phase02.phase2myposbackend.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/item")
 public class ItemController {
+
+    @Autowired
+    private ItemService itemService;
 }
