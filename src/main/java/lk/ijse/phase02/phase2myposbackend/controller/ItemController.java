112f517c8bd7efca61d489ae2d29ef23b259fad2
Charutha-Herath <charuh2023@gmail.com>
2024-10-20 18:33:39 +0530

    add ItemController

---
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/controller/ItemController.java
@@ -0,0 +1,9 @@
+package lk.ijse.phase02.phase2myposbackend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/item")
+public class ItemController {
+}
