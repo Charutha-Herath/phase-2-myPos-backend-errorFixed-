69054dbde3346f4ba15b670cea11fc00e82bbfd9
Charutha-Herath <charuh2023@gmail.com>
2024-10-20 18:20:15 +0530

    done getAllCustomer implementation

--- src/main/java/lk/ijse/phase02/phase2myposbackend/service/CustomerService.java
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/service/CustomerService.java
@@ -3,6 +3,8 @@ package lk.ijse.phase02.phase2myposbackend.service;
 import lk.ijse.phase02.phase2myposbackend.dto.CustomerStatus;
 import lk.ijse.phase02.phase2myposbackend.dto.impl.CustomerDTO;
 
+import java.util.List;
+
 public interface CustomerService {
 
     void saveCustomer(CustomerDTO customerDTO);
@@ -10,4 +12,8 @@ public interface CustomerService {
     CustomerStatus getNextNewId();
 
     void updateCustomer(String custId, CustomerDTO customerDTO);
+
+    void deleteCustomer(String customerId);
+
+    List<CustomerDTO> getAllCustomer();
 }
