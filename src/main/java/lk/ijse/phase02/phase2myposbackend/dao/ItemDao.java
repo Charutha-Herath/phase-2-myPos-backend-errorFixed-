17b0c354d0a8d0e3444768b77cbda11d2f0850c0
Charutha-Herath <charuh2023@gmail.com>
2024-10-20 20:15:51 +0530

    updated ItemDao

--- src/main/java/lk/ijse/phase02/phase2myposbackend/dao/ItemDao.java
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/dao/ItemDao.java
@@ -3,6 +3,8 @@ package lk.ijse.phase02.phase2myposbackend.dao;
 import lk.ijse.phase02.phase2myposbackend.entity.impl.CustomerEntity;
 import lk.ijse.phase02.phase2myposbackend.entity.impl.ItemEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemDao extends JpaRepository<ItemEntity,String> {
 }
