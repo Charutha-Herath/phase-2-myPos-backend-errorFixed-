1d5d14a5c5dcd137530e8b02d80c127116eb852f
Charutha-Herath <charuh2023@gmail.com>
2024-10-20 19:46:55 +0530

    add mapping methods for item process

--- src/main/java/lk/ijse/phase02/phase2myposbackend/util/Mapping.java
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/util/Mapping.java
@@ -3,8 +3,10 @@ package lk.ijse.phase02.phase2myposbackend.util;
 
 
 import lk.ijse.phase02.phase2myposbackend.dto.impl.CustomerDTO;
+import lk.ijse.phase02.phase2myposbackend.dto.impl.ItemDTO;
 import lk.ijse.phase02.phase2myposbackend.dto.impl.UserDTO;
 import lk.ijse.phase02.phase2myposbackend.entity.impl.CustomerEntity;
+import lk.ijse.phase02.phase2myposbackend.entity.impl.ItemEntity;
 import lk.ijse.phase02.phase2myposbackend.entity.impl.UserEntity;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
@@ -24,11 +26,25 @@ public class Mapping {
 
     public UserDTO toUserDTO(UserEntity userEntity) {return modelMapper.map(userEntity, UserDTO.class);}
 
+
+
     public CustomerEntity toCustomerEntity(CustomerDTO customerDTO){return modelMapper.map(customerDTO, CustomerEntity.class);}
 
     public CustomerDTO toCustomerDTO(CustomerEntity customerEntity){return modelMapper.map(customerEntity,CustomerDTO.class);}
 
     public List<CustomerDTO> asCustomerDTOList(List<CustomerEntity> customerEntities) {
         return modelMapper.map(customerEntities, new TypeToken<List<CustomerDTO>>() {}.getType());
+    }
+
+
+
+    public ItemEntity toItemEntity(ItemDTO itemDTO) {
+        return modelMapper.map(itemDTO, ItemEntity.class);
+    }
+
+    public ItemDTO toItemDTO(ItemEntity itemEntity) {return modelMapper.map(itemEntity, ItemDTO.class);}
+
+    public List<ItemDTO> asItemDTOList(List<ItemEntity> itemEntities) {
+        return modelMapper.map(itemEntities, new TypeToken<List<ItemDTO>>() {}.getType());
     }
 }
