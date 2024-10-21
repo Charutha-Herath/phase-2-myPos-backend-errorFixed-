5812774fd0c28cdd7734e3bf542eb1b97d2f8eed
Charutha-Herath <charuh2023@gmail.com>
2024-10-21 23:49:55 +0530

    add getNextNewItemId method

--- src/main/java/lk/ijse/phase02/phase2myposbackend/service/ItemService.java
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/service/ItemService.java
@@ -1,4 +1,8 @@
 package lk.ijse.phase02.phase2myposbackend.service;
 
+
+import lk.ijse.phase02.phase2myposbackend.dto.ItemStatus;
+
 public interface ItemService {
+    ItemStatus getNextNewItemId();
 }
