136bff0e25f1d22560111e3a733f51aba4bde430
Charutha-Herath <charuh2023@gmail.com>
2024-10-21 23:57:29 +0530

    updated CustomerDao

--- src/main/java/lk/ijse/phase02/phase2myposbackend/dao/CustomerDao.java
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/dao/CustomerDao.java
@@ -1,10 +1,9 @@
 package lk.ijse.phase02.phase2myposbackend.dao;
 
 import lk.ijse.phase02.phase2myposbackend.entity.impl.CustomerEntity;
-import lk.ijse.phase02.phase2myposbackend.entity.impl.UserEntity;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
