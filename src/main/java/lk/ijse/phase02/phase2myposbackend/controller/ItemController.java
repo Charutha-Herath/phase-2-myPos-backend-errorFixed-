f2d396f07af78c7911b693f8d77730c0a2407374
Charutha-Herath <charuh2023@gmail.com>
2024-10-21 23:49:17 +0530

    add generateNextItemId method

--- src/main/java/lk/ijse/phase02/phase2myposbackend/controller/ItemController.java
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/controller/ItemController.java
@@ -1,7 +1,10 @@
 package lk.ijse.phase02.phase2myposbackend.controller;
 
+
+import lk.ijse.phase02.phase2myposbackend.dto.ItemStatus;
 import lk.ijse.phase02.phase2myposbackend.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +14,10 @@ public class ItemController {
 
     @Autowired
     private ItemService itemService;
+
+    @GetMapping("generateNextItemId")
+    public ItemStatus generateNewId(){
+        return itemService.getNextNewItemId();
+
+    }
 }
