f8a67a9336f7b4ff4e831653e24788718dd63e4f
Charutha-Herath <charuh2023@gmail.com>
2024-10-21 23:54:52 +0530

    add getNextNewItemId method and its implementation

--- src/main/java/lk/ijse/phase02/phase2myposbackend/service/impl/ItemServiceImpl.java
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/service/impl/ItemServiceImpl.java
@@ -3,6 +3,10 @@ package lk.ijse.phase02.phase2myposbackend.service.impl;
 import jakarta.transaction.Transactional;
 import lk.ijse.phase02.phase2myposbackend.dao.CustomerDao;
 import lk.ijse.phase02.phase2myposbackend.dao.ItemDao;
+
+import lk.ijse.phase02.phase2myposbackend.dto.ItemStatus;
+
+import lk.ijse.phase02.phase2myposbackend.entity.impl.ItemEntity;
 import lk.ijse.phase02.phase2myposbackend.service.ItemService;
 import lk.ijse.phase02.phase2myposbackend.util.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,4 +20,34 @@ public class ItemServiceImpl implements ItemService {
     private ItemDao itemDao;
     @Autowired
     private Mapping mapping;
+
+    @Override
+    public ItemStatus getNextNewItemId() {
+        String Newid = itemDao.generateNextNewId();
+        System.out.println("NewId : "+Newid);
+
+        ItemEntity itemEntity = new ItemEntity();
+        itemEntity.setItemCode(Newid);
+        String id = (itemEntity != null) ? itemEntity.getItemCode() : null;
+
+        if (id == null) {
+            // If no ID exists, return the first
+            return new ItemStatus() {
+                @Override
+                public String getItemId() {
+                    return "Item-0001";
+                }
+            };
+        } else {
+            // Extract the numeric part and increment it
+            int nextId = Integer.parseInt(id.substring(5)) + 1;
+            String newId = "Item-" + String.format("%04d", nextId);
+            return new ItemStatus() {
+                @Override
+                public String getItemId() {
+                    return newId;
+                }
+            };
+        }
+    }
 }
