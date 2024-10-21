8abe6f27e8afafb45555115e97f918c10b7acfec
Charutha-Herath <charuh2023@gmail.com>
2024-10-21 23:56:15 +0530

    add itemDao and itemStatus

--- src/main/java/lk/ijse/phase02/phase2myposbackend/dao/ItemDao.java
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/dao/ItemDao.java
@@ -1,10 +1,13 @@
 package lk.ijse.phase02.phase2myposbackend.dao;
 
-import lk.ijse.phase02.phase2myposbackend.entity.impl.CustomerEntity;
+
 import lk.ijse.phase02.phase2myposbackend.entity.impl.ItemEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ItemDao extends JpaRepository<ItemEntity,String> {
+    @Query(value = "SELECT MAX(itemCode) FROM item", nativeQuery = true)
+    String generateNextNewId();
 }
