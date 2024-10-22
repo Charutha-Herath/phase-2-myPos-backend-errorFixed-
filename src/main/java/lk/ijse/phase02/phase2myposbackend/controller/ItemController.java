b2d65a17c3917847d4b8627b608b2e748c2d7ef4
Charutha-Herath <charuh2023@gmail.com>
2024-10-22 14:04:42 +0530

    add saveItem

--- src/main/java/lk/ijse/phase02/phase2myposbackend/controller/ItemController.java
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/controller/ItemController.java
@@ -2,11 +2,15 @@ package lk.ijse.phase02.phase2myposbackend.controller;
 
 
 import lk.ijse.phase02.phase2myposbackend.dto.ItemStatus;
+import lk.ijse.phase02.phase2myposbackend.dto.impl.CustomerDTO;
+import lk.ijse.phase02.phase2myposbackend.dto.impl.ItemDTO;
+import lk.ijse.phase02.phase2myposbackend.exception.DataPersistException;
 import lk.ijse.phase02.phase2myposbackend.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/item")
@@ -19,5 +23,20 @@ public class ItemController {
     public ItemStatus generateNewId(){
         return itemService.getNextNewItemId();
 
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveItems(@RequestBody ItemDTO itemDTO) {
+        try {
+            itemService.saveItem(itemDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }catch (DataPersistException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
