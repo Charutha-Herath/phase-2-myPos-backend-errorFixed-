ed2df15727a4bc672fc36fdd8c25a71eec8968cc
Charutha-Herath <charuh2023@gmail.com>
2024-10-22 16:59:29 +0530

    add and done PlaceOrderEntity

---
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/entity/impl/PlaceOrderEntity.java
@@ -0,0 +1,29 @@
+package lk.ijse.phase02.phase2myposbackend.entity.impl;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "order")
+public class PlaceOrderEntity implements Serializable {
+
+    @Id
+    @Column(name = "orderId")
+    private String orderId;
+    private String customerId;
+    private Date orderDate;
+    private Double total;
+    private Double discount;
+    private int cash;
+}
