f00aba6778bf934b221b0627ee479850f63fa21b
Charutha-Herath <charuh2023@gmail.com>
2024-10-22 14:55:02 +0530

    add put method for update item

--- src/main/java/lk/ijse/phase02/phase2myposbackend/controller/ItemController.java
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/controller/ItemController.java
@@ -4,8 +4,10 @@ package lk.ijse.phase02.phase2myposbackend.controller;
 import lk.ijse.phase02.phase2myposbackend.dto.ItemStatus;
 import lk.ijse.phase02.phase2myposbackend.dto.impl.CustomerDTO;
 import lk.ijse.phase02.phase2myposbackend.dto.impl.ItemDTO;
+import lk.ijse.phase02.phase2myposbackend.exception.CustomerNotFoundException;
 import lk.ijse.phase02.phase2myposbackend.exception.DataPersistException;
 import lk.ijse.phase02.phase2myposbackend.service.ItemService;
+import lk.ijse.phase02.phase2myposbackend.util.RegexProcess;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -32,6 +34,25 @@ public class ItemController {
             itemService.saveItem(itemDTO);
             return new ResponseEntity<>(HttpStatus.CREATED);
         }catch (DataPersistException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping(value = "/{itemId}")
+    public ResponseEntity<Void> updateCustomer(@PathVariable ("itemId") String itemId,
+                                               @RequestBody ItemDTO updatedItemDto){
+        //validations
+        try {
+            if(!RegexProcess.ItemIdMatcher(itemId) || updatedItemDto == null){
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            itemService.updateItem(itemId,updatedItemDto);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (CustomerNotFoundException e){
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }catch (Exception e){
