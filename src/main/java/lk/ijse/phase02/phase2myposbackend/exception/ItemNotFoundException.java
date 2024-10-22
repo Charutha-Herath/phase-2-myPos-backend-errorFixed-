f90bee26d094b89e07e15576c3c8f103907e87a6
Charutha-Herath <charuh2023@gmail.com>
2024-10-22 14:56:43 +0530

    add ItemNotFoundException

---
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/exception/ItemNotFoundException.java
@@ -0,0 +1,14 @@
+package lk.ijse.phase02.phase2myposbackend.exception;
+
+public class ItemNotFoundException extends RuntimeException{
+
+    public ItemNotFoundException() {
+    }
+
+    public ItemNotFoundException(String message) {
+
+    }
+    public ItemNotFoundException(String message, Throwable cause) {
+
+    }
+}
