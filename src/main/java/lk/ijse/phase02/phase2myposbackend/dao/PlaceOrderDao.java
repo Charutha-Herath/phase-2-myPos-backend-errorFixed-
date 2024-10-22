f8d99fc8cfaa4ad263c06cccfb814178cb1af16d
Charutha-Herath <charuh2023@gmail.com>
2024-10-22 20:27:33 +0530

    add native query for generateNextNewId

--- src/main/java/lk/ijse/phase02/phase2myposbackend/dao/PlaceOrderDao.java
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/dao/PlaceOrderDao.java
@@ -3,8 +3,11 @@ package lk.ijse.phase02.phase2myposbackend.dao;
 import lk.ijse.phase02.phase2myposbackend.entity.impl.CustomerEntity;
 import lk.ijse.phase02.phase2myposbackend.entity.impl.PlaceOrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PlaceOrderDao extends JpaRepository<PlaceOrderEntity,String> {
+    @Query(value = "SELECT MAX(orderId) FROM orders", nativeQuery = true)
+    String generateNextNewId();
 }
