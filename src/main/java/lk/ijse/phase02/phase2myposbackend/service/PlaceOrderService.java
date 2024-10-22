e25a00f4cd4aad3e0ed352b62e85128da58e6e16
Charutha-Herath <charuh2023@gmail.com>
2024-10-22 20:29:08 +0530

    add method PlaceOrderService

--- src/main/java/lk/ijse/phase02/phase2myposbackend/service/PlaceOrderService.java
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/service/PlaceOrderService.java
@@ -1,8 +1,9 @@
 package lk.ijse.phase02.phase2myposbackend.service;
 
 import lk.ijse.phase02.phase2myposbackend.dto.ItemStatus;
+import lk.ijse.phase02.phase2myposbackend.dto.PlaceOrderStatus;
 
 public interface PlaceOrderService {
 
-    ItemStatus getNextNewOrderId();
+    PlaceOrderStatus getNextNewOrderId();
 }
