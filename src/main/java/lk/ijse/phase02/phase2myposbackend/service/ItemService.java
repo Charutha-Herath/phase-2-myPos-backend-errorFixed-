28d7c4c0ece4625feb92ba6aa64fc9c7ff724a41
Charutha-Herath <charuh2023@gmail.com>
2024-10-22 14:05:16 +0530

    add saveItem

--- src/main/java/lk/ijse/phase02/phase2myposbackend/service/ItemService.java
+++ src/main/java/lk/ijse/phase02/phase2myposbackend/service/ItemService.java
@@ -2,7 +2,11 @@ package lk.ijse.phase02.phase2myposbackend.service;
 
 
 import lk.ijse.phase02.phase2myposbackend.dto.ItemStatus;
+import lk.ijse.phase02.phase2myposbackend.dto.impl.CustomerDTO;
+import lk.ijse.phase02.phase2myposbackend.dto.impl.ItemDTO;
 
 public interface ItemService {
     ItemStatus getNextNewItemId();
+
+    void saveItem(ItemDTO itemDTO);
 }
